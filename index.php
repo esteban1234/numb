237f3ac84f190f5ffe157a1231be8a55fbbe7e3d
esteban1234 <tva_libra@hotmail.com>
2017-09-29 16:18:36 -0500

    pagina lista

---
+++ index.php
@@ -0,0 +1,281 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    
+    <link rel="icon" type="image/png" href="img/favicon.png" />
+    <title>SERVICIOS DE LIMPIEZA NUMB</title>
+   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <META NAME="Title" content="SERVICIOS DE LIMPIEZA NUMB">
+    <META NAME="Author" CONTENT="[http://serviciosnumb.com/]"/>
+    <META name="Description" content="Somos una compañía ubicada en la ciudad de mérida, mérida, dedicados a ofrecer servicios de limpieza."/>
+    <META NAME="Keywords" CONTENT="Limpieza en mérida, Limpieza, organización de Limpieza, Limpieza en mérida yucatan, Limpieza de empresas y hogares, hogares, empresas y hogares."/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/iconos.min.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsivo.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <script src="js/general.js"></script>
+
+
+
+    <script src="js/smooth-scroll.min.js"></script>
+    <script>
+      smoothScroll.init({
+      selector: '[data-scroll]',
+      selectorHeader: null,
+      speed: 2000,
+      easing: 'easeInOutCubic',
+      offset: 0,
+      callback: function ( anchor, toggle ) {}
+      });
+    </script>
+
+  </head>
+  <body>
+<div class="bannerh" id="INICIO">
+  <header>
+    <figure class="logo pull-left">
+      <img src="img/numb.jpg" class="img-responsive" alt="NUMB">
+    </figure>
+    <nav class="pull-right">
+      <ul>
+        <a class="selector" id="select" data-scroll href="#INICIO" aria-pressed="true"><span class="iconom"><i class="fa fa-home" aria-hidden="true"></i></span> <span class="">INICIO</span></a>
+        <a class="selector" data-scroll href="#NOSOTROS" aria-pressed="true">NOSOTROS</a>
+        <a class="selector" data-scroll href="#SERVICIOS" aria-pressed="true">SERVICIOS</a>
+        <a class="selector" data-scroll href="#CONTACTO" aria-pressed="true">CONTACTO</a>
+      </ul>
+    </nav>
+  </header>
+
+  <div class="contentextbanner">
+    <p class="textob pull-right">NOS ENCARGAMOS DE LA <br> LIMPIEZA DE SU NEGOCIO</p>
+      <div class="botonb pull-right" style="text-align: center; margin-top: 5%;">
+        <a class="btn btn-success btn-outline btn-lg" data-scroll href="#CONTACTO">¡CONTACTANOS!</a>
+      </div>
+  </div>
+</div>
+
+
+
+  <span class="ir-arriba"><i class="fa fa-arrow-up" aria-hidden="true"></i></span>
+
+<section class="seccion1" id="NOSOTROS">
+<h1 class="tit">NOSOTROS</h1>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <figure class="imagen">
+              
+            </figure>
+            <h2 class="text-center" style="color: #429F73;">Limpieza de Calidad</h2>
+            
+          </div>
+          <div class="col-md-4">
+            <figure class="imagen2">
+              
+            </figure>
+            <h2 class="text-center" style="color: #429F73;">Personal Profesional</h2>
+            
+          </div>
+          <div class="col-md-4">
+            <figure class="imagen3">
+              
+            </figure>
+            <h2 class="text-center" style="color: #429F73;">Precios Económicos</h2>
+            
+          </div>
+        </div>
+      </div>
+      <div class="banner2">
+        <div class="fondo">
+          <p>Ofrecemos servicios de limpieza a precios accesibles <br> para que no tenga que preocuparse por la higiene de su organización.</p>
+        </div>
+      </div>
+
+      <div class="container dos">
+        <div class="row">
+          <div class="col-md-4">
+            <figure class="imagen1">
+              <img src="img/objetivo.png" alt="">
+            </figure>
+            <h2 class="text-center" style="color: #429F73;">NOSOTROS</h2>
+            <p class="text-center">Contamos con personal altamente capacitado en técnicas y manejo de productos de limpieza con lo que garantizamos un cuidado óptimo de los materiales utilizados en la arquitectura y decoración de su empresa. </p>
+          </div>
+          <div class="col-md-4">
+            <figure class="imagen21">
+              <img src="img/mision.png" alt="">
+            </figure>
+            <h2 class="text-center" style="color: #429F73;">MISION</h2>
+            <p class="text-center">Brindar un servicio de limpieza de la alta calidad a nuestros clientes, proporcionánlo de manera eficaz y honesta, para logar una completa satisfación y lealtad a la empresa.</p>
+          </div>
+          <div class="col-md-4">
+            <figure class="imagen31">
+              <img src="img/vision.png" alt="">
+            </figure>
+            <h2 class="text-center" style="color: #429F73;">VISION</h2>
+            <p class="text-center">Llegar a ser la empresa de limpieza mas reconocida, brindandoles un servicio de calidad y profesional a traves de tener los colaboradores mas capacitados y motivados en el ramo.</p>
+          </div>
+        </div>
+      </div>
+</section>
+
+<section class="seccion2" id="SERVICIOS">
+    <h1 class="tit">SERVICIOS</h1>
+    <p class="text-center">Trabajar en un ambiente limpio, fresco e higiénico incrementa la productividad y genera bienestar en nuestros colaboradores.</p>
+    <br><br>
+    <div class="container">
+        <div class = "row">
+            <div class = "col-md-4">
+                <div class="card-base">
+                  <div class="card-icon"><a href="#" title="Widgets" id="widgetCardIcon" class="imagecard"><span class="fa fa-cog fa-spin fa-fw"></span></a>
+                      <div class="card-data widgetCardData"><br>
+                      <h3 class="box-title" style="color: #bb7824;">LIMPIEZA INSTITUCIONAL</h3> <br><br>
+                      <p class="card-block text-center">Personal eventual o permanente capacitado para el aseo de sus oficinas.</p>
+                    <!-- <a href="#" title="Style Builder" class="anchor btn btn-default" style="background: #bb7824; border: #bb7824;"> <i class="fa fa-paper-plane" aria-hidden="true"></i>  Explore More </a> --></div>
+                  </div>
+                  <div class="space"></div>
+                </div>
+            </div>
+
+            <div class = "col-md-4">
+                <div class="card-base">
+                  <div class="card-icon"><a href="#" title="Widgets" id="widgetCardIcon" class="imagecard"><span class="fa fa-cog fa-spin fa-fw"></span></a>
+                      <div class="card-data widgetCardData"><br>
+                      <h3 class="box-title" style="color: #bb7824;">CENTROS EDUCATIVOS</h3><br><br>
+                      <p class="card-block text-center">Controles, reportes, procesos y roles de trabajo para satisfacer las necesidades de nuestros clientes.</p>
+                    <!-- <a href="#" title="Style Builder" class="anchor btn btn-default" style="background: #bb7824; border: #bb7824;"> <i class="fa fa-paper-plane" aria-hidden="true"></i>  Explore More </a> --></div>
+                  </div>
+                  <div class="space"></div>
+                </div>
+            </div>
+
+            <div class = "col-md-4">
+                <div class="card-base">
+                  <div class="card-icon"><a href="#" title="Widgets" id="widgetCardIcon" class="imagecard"><span class="fa fa-cog fa-spin fa-fw"></span></a>
+                      <div class="card-data widgetCardData"><br>
+                      <h3 class="box-title" style="color: #bb7824;">LIMPIEZA INDUSTRIAL</h3><br><br>
+                      <p class="card-block text-center">Equipos especializados y productos químicos idóneos para limpieza industrial 100% biodegradables.</p>
+                    <!-- <a href="#" title="Style Builder" class="anchor btn btn-default" style="background: #bb7824; border: #bb7824;"> <i class="fa fa-paper-plane" aria-hidden="true"></i>  Explore More </a> --></div>
+                  </div>
+                  <div class="space"></div>
+                </div>
+            </div>
+        </div> <br><br>
+
+        <div class = "row">
+            <div class = "col-md-4">
+                <div class="card-base">
+                  <div class="card-icon"><a href="#" title="Widgets" id="widgetCardIcon" class="imagecard"><span class="fa fa-cog fa-spin fa-fw"></span></a>
+                      <div class="card-data widgetCardData"><br>
+                      <h3 class="box-title" style="color: #bb7824;">CENTROS DE SALUD</h3><br><br>
+                      <p class="card-block text-center">Personal capacitado para limpiar y sanitizar correctamente quirofanos, habitaciones, salas de espera y areas comunes.</p>
+                    <!-- <a href="#" title="Style Builder" class="anchor btn btn-default" style="background: #bb7824; border: #bb7824;"> <i class="fa fa-paper-plane" aria-hidden="true"></i>  Explore More </a> --></div>
+                  </div>
+                  <div class="space"></div>
+                </div>
+            </div>
+
+            <div class = "col-md-4">
+                <div class="card-base">
+                  <div class="card-icon"><a href="#" title="Widgets" id="widgetCardIcon" class="imagecard"><span class="fa fa-cog fa-spin fa-fw"></span></a>
+                      <div class="card-data widgetCardData"><br>
+                      <h3 class="box-title" style="color: #bb7824;">CENTROS COMERCIALES</h3><br><br>
+                      <p class="card-block text-center">Somos especialistas en proveer suministros adecuados para lograr un ambiente limpio y comodo para sus usuarios.</p>
+                   <!--  <a href="#" title="Style Builder" class="anchor btn btn-default" style="background: #bb7824; border: #bb7824;"> <i class="fa fa-paper-plane" aria-hidden="true"></i>  Explore More </a> --></div>
+                  </div>
+                  <div class="space"></div>
+                </div>
+            </div>
+
+            <div class = "col-md-4">
+                <div class="card-base">
+                  <div class="card-icon"><a href="#" title="Widgets" id="widgetCardIcon" class="imagecard"><span class="fa fa-cog fa-spin fa-fw"></span></a>
+                      <div class="card-data widgetCardData"><br>
+                      <h3 class="box-title" style="color: #bb7824;">INDUSTRIA ALIMENTICIA</h3><br><br>
+                      <p class="card-block text-center">Ofrecemos a nuestros clientes químicos especializados para sanitizar sus equipos y mesas de trabajo. Todos de grado alimenticio. </p>
+                    <!-- <a href="#" title="Style Builder" class="anchor btn btn-default" style="background: #bb7824; border: #bb7824;"> <i class="fa fa-paper-plane" aria-hidden="true"></i>  Explore More </a> --></div>
+                  </div>
+                  <div class="space"></div>
+                </div>
+            </div>
+        </div>
+  
+</div>
+</section>
+
+<section class="seccion3" id="CONTACTO">
+  <h1 class="tit">CONTACTO</h1>
+       <div class="cont">
+          <ul class="contact">
+            <li style="text-align: left;">
+              <p><i class="fa fa-map-marker fa-3x"></i></p>
+              <p><strong>Dirección</strong></br>C. 26-D No162-A, por C. 23 y C. 25 <br> Col. Chuburna de Hidalgo, Mérida, Yucatán. <br> C.P. 97205</p>
+            </li>
+            <li style="text-align: left; vertical-align: top;">
+              <p><i class="fa fa-phone fa-3x"></i></p>
+              <p><strong>Teléfono</strong></br>PENDIENTE</p>
+            </li>
+            <li style="text-align: left; vertical-align: top;">
+              <p><i class="fa fa-envelope fa-3x"></i></p>
+              <p><strong>E-mail</strong></br>info@serviciosnumb.com</p>
+            </li>
+            <li style="text-align: left; vertical-align: top;">
+              <p><i class="fa fa-clock-o fa-3x"></i></p>
+              <p><strong>Horario</strong></br>Lunes a Viernes de 9 am a 6 pm.</p>
+            </li>
+          </ul>
+      </div>
+      <br><br>
+
+<div class="container">
+              <div class="row">
+              <div class="col-sm-6">
+              <div class="mapa">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.5655678782737!2d-89.64126288596!3d21.010044593794824!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f567411fe5d1ef9%3A0x651ff377a33e9cc9!2sCalle+26%E1%B4%B0+162A%2C+Chuburn%C3%A1+de+Hidalgo%2C+97205+M%C3%A9rida%2C+Yuc.!5e0!3m2!1ses-419!2smx!4v1504217768923" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+              </div>
+              </div>
+                  
+              <div class="col-sm-6 contact-form">
+              <legend>No dudes en ponerte en contacto, sus comentarios son importantes para nosotros</legend> <br><br>
+              <form id="contact" method="post" class="form" role="form">
+              <div class="row">
+              <div class="col-xs-6 col-md-4 form-group">
+              <input class="form-control" id="name" name="name" placeholder="Nombre Completo" type="text" required autofocus />
+              </div>
+              <div class="col-xs-6 col-md-4 form-group">
+              <input class="form-control" id="email" name="email" placeholder="Escribe tu correo" type="email" required />
+              </div>
+              <div class="col-xs-6 col-md-4 form-group">
+              <input class="form-control" id="email" name="email" placeholder="Escribe tu teléfono" type="text" required />
+              </div>
+              </div>
+              <textarea class="form-control" id="message" name="message" placeholder="Escribe tu comentario" rows="5"></textarea>
+              <br />
+              <div class="row">
+              <div class="col-xs-12 col-md-12 form-group">
+              <button class="btn btn-success pull-right" type="submit">Enviar</button>
+              </form>
+              </div>
+              </div>
+</div>
+ 
+</section>
+
+
+
+<footer>
+<p>Derechos de autor 2017 © | SERVICIOS DE LIMPIEZA NUMB S.C.P </p>
+</footer>
+
+    <script src="js/sendCORREO.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js" integrity="sha384-XTs3FgkjiBgo8qjEjBk0tGmf3wPrWtA6coPfQDfFEY8AnYJwjalXCiosYRBIBZX8" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.js"></script>
+    <script src="js/arriba.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/menu.js"></script>
+  </body>
+</html>
